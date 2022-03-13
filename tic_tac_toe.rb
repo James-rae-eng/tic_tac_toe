f3af56f3cc4b8354ed3394cba4c92df6e1e1650c
James Rae <jrae151515@yahoo.co.uk>
2022-03-13 17:52:48 +0000

    Updated overall layout to include classes for; users, the playing board, and the game itself. Reconfigured to capture 2 users input and show an example board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,30 +1,52 @@
-puts "Lets play a game of Tic-Tac-Toe!"
+class User
+    attr_reader :name, :symbol
 
-def assign_name(number)
-    puts "Please enter player #{number}'s name."
-    gets
-end
-def assign_symbol(number)
-    puts "Please enter player #{number}'s symbol (e.g o or x)."
-    gets
-end
-
-def example_grid
-    puts "The board will be displayed as follows, please choose your selection by entering its corresponding number."
-    num_grid = [["1", "|", "2", "|", "3"],["4", "|", "5", "|", "6"],["7", "|", "8", "|", "9"]]
-    puts num_grid[0].join, num_grid[1].join, num_grid[2].join
-end
-
-class Game
     def initialize(name, symbol)
         @name = name
         @symbol = symbol
     end
-    
-    @@grid = [["-", "-", "-"],["-", "-", "-"],["-", "-", "-"]]
-    puts @@grid[0].join("|"), @@grid[1].join("|"), @@grid[2].join("|")
+end
+
+class Grid
+
+    def example_board
+        puts "The board will be displayed as follows, please choose your selection by entering its corresponding number."
+        num_grid = [["1", "|", "2", "|", "3"],["4", "|", "5", "|", "6"],["7", "|", "8", "|", "9"]]
+        puts num_grid[0].join, num_grid[1].join, num_grid[2].join
+    end
+
+    @@board = [["-", "-", "-"],["-", "-", "-"],["-", "-", "-"]]
+    #puts @@board[0].join("|"), @@board[1].join("|"), @@board[2].join("|")
+
+    def update_board(number)
+        #take player selection and apply to board
+        
+    end
+
+end
+
+
+class Game 
+    attr_reader :board
+
+    def initialize
+        @board = Grid.new
+    end
+
+    def create_user(number)
+        puts "Please enter player #{number}'s name."
+        name = gets.chomp
+        puts "Please enter player #{number}'s symbol (e.g o or x)."
+        symbol = gets.chomp
+        User.new(name, symbol)
+    end
+
 
     def play_round(number, symbol)
+        #take player selection and apply to board
+        #display updated board
+        #check if update is game over (and say if it is)
+        #loop to next player if not game over
         if number <= 3
             adjusted = number - 1
             @@grid[0][adjusted] = symbol
@@ -38,12 +60,21 @@ class Game
             puts "Invalid input"
         end
         puts @@grid[0].join("|"), @@grid[1].join("|"), @@grid[2].join("|")
+        
+    
     end
 
+    def play
+        puts "Lets play a game of Tic-Tac-Toe!"
+        user_1 = create_user(1)
+        user_2 = create_user(2)
+        board.example_board
+        #player (random to start then alternate) make a selection (loop)
+        #when game is over (win or board full) display winner
+        #Restart game?
+    end
 end  
-player_one = Game.new(assign_name(1), assign_symbol(1))
-player_two = Game.new(assign_name(2), assign_symbol(2))
 
-example_grid()
+game = Game.new
+game.play
 
-player_one.play_round(2, "x")
