5b1a3b21b6f0f80fba2307566d294b87027e1ee9
James Rae <jrae151515@yahoo.co.uk>
2022-03-24 17:31:59 +0000

    added final functionality, including restarting of game and error catching for symbols and selections

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,6 +11,7 @@ class Grid
     def initialize
         @board = [["-", "-", "-"],["-", "-", "-"],["-", "-", "-"]]
         @winning_combo = [[1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7], [1,4,7], [2,5,8], [3,6,9]]
+        @board_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     end
 
     def example_board
@@ -33,6 +34,7 @@ class Grid
         else
             puts "Invalid input"
         end
+        @board_positions.delete(number)
         puts @board[0].join("|"), @board[1].join("|"), @board[2].join("|")
     end
 
@@ -44,7 +46,11 @@ class Grid
     end
 
     def board_full?
-        @board.flatten.include?("-") ? false : true
+        @board_positions.empty?
+    end
+
+    def valid_selection(selection)
+        @board_positions.include?(selection)
     end
 end
 
@@ -60,8 +66,19 @@ class Game
     def create_user(number)
         puts "Please enter player #{number}'s name."
         name = gets.chomp
-        puts "Please enter player #{number}'s symbol (e.g o or x)."
-        symbol = gets.chomp
+        puts "player #{number}, please enter a single character to be your game marker (e.g o or x)."
+        while symbol = gets.chomp
+            case 
+            when symbol == "-" || symbol == ""
+                puts "Invalid selection, please choose a different character"
+            when symbol.length >= 2
+                puts "Please choose a single character"
+            when number == 2 && symbol == @user_1.symbol
+                puts "player 1 has already chosen that character, please choose again"
+            else 
+                break
+            end
+        end
         User.new(name, symbol)
     end
 
@@ -69,7 +86,14 @@ class Game
         #take player selection, apply it to board & their selections array
         #then check if game is over and swap player if not
         puts "#{user.name} select your position"
-        @selection = gets.chomp.to_i
+        while @selection = gets.chomp.to_i
+            case
+            when board.valid_selection(@selection) == false
+                puts "invalid selection, please choose again"
+            else
+                break
+            end
+        end
         user.selections.push(@selection)
         board.update_board(@selection, user.symbol)
         game_over?()
@@ -96,13 +120,25 @@ class Game
         end
     end
 
+    def replay_game
+        puts "Game over, would you like to play again? Enter y / n"
+        choice = gets.chomp.to_s.downcase
+        if choice == "y"
+            game = Game.new
+            game.play
+        elsif choice == "n"
+            puts "Thanks for playing!"
+        else
+            puts "Invalid selection"
+        end
+    end
+
     def play
         puts "Lets play a game of Tic-Tac-Toe!"
         @user_1 = create_user(1)
         @user_2 = create_user(2)
         board.example_board
-        #CHANGE BELOW TO RANDOM START
-        @current_user = @user_1
+        @current_user = [@user_1, @user_2].sample
 
         while game_over?() == false
             play_round(@current_user)
@@ -113,11 +149,10 @@ class Game
         elsif board.winning_combo?(@current_user) == true
             puts "#{@current_user.name} you win!"
         end
-      
-        #Restart game?
+        
+        replay_game()
     end
 end  
 
 game = Game.new
 game.play
-
